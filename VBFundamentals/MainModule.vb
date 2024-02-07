e6f93b8d78ac61b3677936e33a0dd5f2b3aad968
KBeKind <79383539+KBeKind@users.noreply.github.com>
2024-02-06 18:30:13 -0600

    commit

--- VBFundamentals/MainModule.vb
+++ VBFundamentals/MainModule.vb
@@ -5,57 +5,110 @@ Module MainModule
 
 
     Dim ProductID As Integer = 1
-    Dim Name As String = "10 Speed Bike"
+
     Dim SellStartDate As DateTime = #1/1/2019#
 
 
     Sub Main()
 
         Dim isActive As Boolean = ClassConstants.DEFAULT_ACTIVE
+
+        'Dim theData As Object
+
+        'theData = "Bike"
+        'Console.WriteLine(theData)
+
+        'theData = 999.99
+        'Console.WriteLine(theData)
+
+        'theData = DateTime.Now
+        'Console.WriteLine(theData)
+
+        'theData = vbEmpty
+        'Console.WriteLine(theData)
+
+        'theData = DBNull.Value
+        'Console.WriteLine(theData)
+
+
+
+        'If True Then
+        '    Dim Name As String = "Tricycle"
+        '    Dim ListPrice As Decimal = 999.99D
+        'End If
+
+        'IncrementListPrice()
+        'IncrementListPrice()
+        'IncrementListPrice()
+
+        Dim Name As String = "10 Speed Bike"
+
+
+        Console.WriteLine("built-in String methods")
+        Console.WriteLine(Name.Length)
+        Console.WriteLine(Name.IndexOf(" "))
+        Console.WriteLine(Name.LastIndexOf(" "))
+        Console.WriteLine(Name.EndsWith("e"))
+        Console.WriteLine(Name.Insert(9, "Mountain "))
+        Console.WriteLine(Name.Remove(0, 9))
+        Console.WriteLine(Name.Replace("10", "12"))
+        Console.WriteLine(Name.ToUpper())
+        Console.WriteLine(Name.ToLower())
+        Console.WriteLine()
+
         Dim ListPrice As Decimal = ClassConstants.DEFAULT_LIST_PRICE
+        Console.WriteLine("built-in Numeric methods")
+        Console.WriteLine(ListPrice.Equals(999.99D))
+        Console.WriteLine(Decimal.MinValue)
+        Console.WriteLine(Decimal.MaxValue)
+        Console.WriteLine(Decimal.Ceiling(ListPrice))
+        Console.WriteLine(Decimal.Floor(ListPrice))
+        Decimal.TryParse("55.01", ListPrice)
+        Console.WriteLine(ListPrice)
+        Decimal.TryParse("Fifty-Five", ListPrice)
+        Console.WriteLine(ListPrice)
+        Console.WriteLine()
+
+        'Dim SellDate As DateTime = #1/1/2024 12:05:01 PM#
+        'Console.WriteLine("built-in DateTime Methods")
+        'Console.WriteLine(SellDate.AddDays(10))
+        'Console.WriteLine(SellDate.AddDays(-10))
+        'Console.WriteLine(SellDate.AddYears(1))
+        'Console.WriteLine(SellDate.Date)
+        'Console.WriteLine(SellDate.Day)
+        'Console.WriteLine(SellDate.DayOfWeek)
+        'Console.WriteLine(SellDate.Year)
+        'Console.WriteLine(SellDate.DayOfYear)
+        'Console.WriteLine(SellDate.Hour)
+        'Console.WriteLine(SellDate.Minute)
+        'Console.WriteLine(SellDate.Second)
+        'Console.WriteLine()
+
+
+        Dim prod As New Product
+
+        prod.SellStartDate = #1/1/2024#
+        Dim sellDate As DateTime
+
+        'prod.CalculateSellEndDate(20, sellDate)
+
+        sellDate = prod.CalculateSellEndDate(20)
+
+        Console.WriteLine(sellDate)
 
 
 
-
-        Dim theData As Object
-
-        theData = "Bike"
-        Console.WriteLine(theData)
-
-        theData = 999.99
-        Console.WriteLine(theData)
-
-        theData = DateTime.Now
-        Console.WriteLine(theData)
-
-        theData = vbEmpty
-        Console.WriteLine(theData)
-
-        theData = DBNull.Value
-        Console.WriteLine(theData)
-
-
-
-        If True Then
-            Dim Name As String = "Tricycle"
-            Dim ListPrice As Decimal = 999.99D
-        End If
-
-
-        IncrementListPrice()
-        IncrementListPrice()
-        IncrementListPrice()
 
         Console.ReadKey()
     End Sub
 
 
     Sub IncrementListPrice()
-        Static ListPrice As Decimal = 0
+        'Static ListPrice As Decimal = 0
 
-        ListPrice = ListPrice + 1
+        'ListPrice = ListPrice + 1
 
-        Console.WriteLine(ListPrice)
+        'Console.WriteLine(ListPrice)
     End Sub
 
 
---
+++ VBFundamentals/Product.vb
@@ -0,0 +1,62 @@
+﻿Public Class Product
+
+    'Private _IsActive As Boolean
+    'Public Property IsActive() As Boolean
+    '    Get
+    '        Return _IsActive
+    '    End Get
+    '    Set(ByVal value As Boolean)
+    '        _IsActive = value
+    '    End Set
+    'End Property
+
+    'Private _Name As String
+    'Public Property Name() As String
+    '    Get
+    '        Return _Name
+    '    End Get
+    '    Set(ByVal value As String)
+    '        _Name = value
+    '    End Set
+    'End Property
+
+    'Private _ProductNumber As String
+    'Public Property ProductNumber() As String
+    '    Get
+    '        Return _ProductNumber
+    '    End Get
+    '    Set(ByVal value As String)
+    '        _ProductNumber = value
+    '    End Set
+    'End Property
+
+
+    Public Property IsActive As Boolean
+    Public Property Name As String
+    Public Property ProductNumber As String
+
+    Public ReadOnly Property NameAndNumber() As String
+        Get
+            Return Name + "-" + ProductNumber
+        End Get
+    End Property
+
+    Public Property SellStartDate As DateTime
+    Public Property SellEndDate As DateTime
+
+
+    'Sub CalculateSellEndDate(ByVal days As Integer)
+    '    SellEndDate = SellStartDate.AddDays(days)
+    'End Sub
+
+    'Sub CalculateSellEndDate(ByVal days As Integer, ByRef sellDate As DateTime)
+    '    SellEndDate = SellStartDate.AddDays(days)
+    '    sellDate = SellEndDate
+    'End Sub
+
+    Function CalculateSellEndDate(ByVal days As Integer) As DateTime
+        SellEndDate = SellStartDate.AddDays(days)
+        Return SellEndDate
+    End Function
+
+End Class
